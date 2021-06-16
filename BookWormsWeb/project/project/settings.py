9bb9b268a350aa2ba895f1eebb86fca5342396c6
Kaiser Samrat <kaisersamrat136@gmail.com>
2021-06-16 22:03:20 +0600

    requirements added

--- BookWormsWeb/project/project/settings.py
+++ BookWormsWeb/project/project/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 TEMPLATES_DIR= os.path.join(BASE_DIR,'templates')
 STATIC_DIR= os.path.join(BASE_DIR,'static')
 MEDIA_DIR=os.path.join(BASE_DIR,'media')
